e2ebe8c9b0196620672f5e1a5c8071c19a1da973
Im-Rises <quentin-morel88@hotmail.com>
2022-06-06 19:28:20 +0200

    Updated Readme and model converter

--- converter.py
+++ converter.py
@@ -1,10 +1,13 @@
 import tensorflowjs
 from keras.models import load_model
+import sys
 
 # https://www.tensorflow.org/js/tutorials/conversion/import_keras
-
 # Convert h5 model+weights from tensorflowpython to tensorflowjs
 if __name__ == "__main__":
-    model = load_model("resnet50_model_weights_ferplus_v2.h5")
-    model.summary()
-    tensorflowjs.converters.save_keras_model(model, "resnet50js_ferplus")
+    if len(sys.argv) < 3:
+        print("Usage : py ./converter.py <yourModel.h5> <nameOfYourTensorflowjsModel> ")
+    else:
+        model = load_model(sys.argv[1])
+        model.summary()
+        tensorflowjs.converters.save_keras_model(model, sys.argv[2])
